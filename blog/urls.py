d453fd82086a5ab0abc619f7085ecdb60f6147e0
Tejas Falke <tejasf4@gmail.com>
2026-01-04 23:36:53 +0530

    connecting new app 'users' to project

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('posts.urls'))
+    path('', include('posts.urls')),
+    path('', include('users.urls'))
 ]
 
 if settings.DEBUG:
