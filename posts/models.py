538ef54a992cd9a886832a2662ebfbad7668c7e8
Tejas Falke <tejasf4@gmail.com>
2026-01-04 13:51:55 +0530

    creating models for tags

--- posts/models.py
+++ posts/models.py
@@ -9,11 +9,20 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
 class BlogPost(models.Model):
     # col_name_in_DB = data type
     title = models.CharField(max_length=100)
     content = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
     image = models.ImageField(upload_to='image/', blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
